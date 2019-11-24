34d2de9871262962ec5e28d5e88b6fbea11c555e
nazzrrg <nazarov.egor2010@yandex.ru>
2019-11-24 14:09:56 +0300

    created a new rust script

---
+++ Tasks/5/target/debug/deps/lab5-8da9cc9021866856.d
@@ -0,0 +1,5 @@
+/Users/nazzrrg/Git/Digital-Culture-Introduction-I/Tasks/5/target/debug/deps/lab5-8da9cc9021866856: src/main.rs
+
+/Users/nazzrrg/Git/Digital-Culture-Introduction-I/Tasks/5/target/debug/deps/lab5-8da9cc9021866856.d: src/main.rs
+
+src/main.rs:
---
+++ Tasks/5/target/debug/lab5.d
@@ -0,0 +1,1 @@
+/Users/nazzrrg/Git/Digital-Culture-Introduction-I/Tasks/5/target/debug/lab5: /Users/nazzrrg/Git/Digital-Culture-Introduction-I/Tasks/5/src/main.rs
