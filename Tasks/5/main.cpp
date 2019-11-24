ecf10ac67e7044ec9227b2efc4bc37c9446ff97e
nazzrrg <nazarov.egor2010@yandex.ru>
2019-11-24 17:07:43 +0300

    Created graph related types

--- Tasks/5/main.cpp
+++ Tasks/5/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <vector>
+#include <map>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+typedef map<int, vector<int>> AdjacencyList;
+typedef vector<pair<int, int>> EdgesList;
+
+AdjacencyList get_adjacency_list(const EdgesList& input) {
+    map<int, vector<int>> result;
+
+    for (auto it : input) {
+        result.insert(it.first, it.second);
+    }
+
+    return result;
+}
+
+int main(int argc, char** argv) {
+    freopen(argv[0], "r", stdin);
+
+    EdgesList edges_list;
+    int from, to;
+    while (cin >> from >> to) {
+        edges_list.emplace_back(from, to);
+    }
+
+    AdjacencyList adjacency_list = get_adjacency_list(edges_list);
+
+
+
     return 0;
 }
